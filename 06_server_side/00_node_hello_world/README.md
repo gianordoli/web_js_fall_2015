2fc6f288a7ae493d4615104ca4f728e3e9f5d258
gianordoli <gianordoligabriel@yahoo.com.br>
2015-10-01 21:23:43 -0400

    Node Hello World

---
+++ 06_server_side/00_node_hello_world/README.md
@@ -0,0 +1,10 @@
+# NodeJS Instructions
+
+* Download NodeJS from the [website](https://nodejs.org/en/) and install it.
+* Make a copy of this folder into your personal one.
+* Open your Terminal and cd to this folder.
+* Type ```npm init```.
+* Keep pressing Enter until it finishes asking for info.
+* Type ```node app.js```.
+* Open your browser and go to ```http://localhost:1337```.
+* You should see a "Hello World" message.
---
+++ _in_class/04_databases/02a_parse_crud/js/main.js
@@ -0,0 +1,144 @@
+/* Your code starts here */
+
+var app = app || {};
+
+app.main = (function() {
+
+	// Declaring our app variables
+	var nameOfTheClass,	// String
+		tvShowsClass,	// Parse Class Object
+		tvShows;		// Instance of tvShowsClass
+	
+	var showQuery;		// Parse Fetch Object
+
+	function initParse(){
+	    Parse.initialize("DIRiJuDfvVZ9y2uXhUrCKpSPlNthjpfifXGgb6Am",	// App ID
+	    				 "hnNNUvneDBzyZf4w9sflSywp1Pfvfy2IuT4d1u7C");	// JS Key
+		nameOfTheClass = 'TV';
+		// 1. CREATE
+		tvShowsClass = Parse.Object.extend(nameOfTheClass);	// Parse Class Object
+		tvShows = new tvShowsClass();						// Instance of
+
+		// 2. READ
+		showQuery = new Parse.Query(nameOfTheClass);
+	}
+
+	// 1. CREATE
+	// A function to save new objects
+	// (I made up this one! Not a Parse thing)
+	var saveShow = function(parseClass, obj){
+		// All the lines below are default Parse functions/syntax
+		parseClass.save(obj, {
+			success: function(res){
+				console.log('Yay! Just saved a new object!');
+			},
+			error: function(res, err){
+				console.log(err);
+			}
+		});
+	};
+
+	// 2. READ
+    var fetchAllShows = function(parseFetch){
+    	parseFetch.find({
+    		success: function(obj){
+    			console.log(obj);
+    		},
+    		error: function(err){
+    			console.log(err);
+    		}
+    	});
+    };
+
+    // 3. READ with conditions
+    function fetchShow(property, value, parseFetch){
+    	parseFetch.greaterThanOrEqualTo(property, value).find({
+    		success: function(obj){
+    			console.log(obj);
+    			obj.forEach(function(item){
+    				// The structure of the object is...
+    				// console.log(item.attributes.title);
+
+    				//I can also call the Parse function .get(attr)
+    				console.log(item.get('title'));
+    			});
+    		},
+    		error: function(err){
+    			console.log(err);
+    		}
+    	});
+	};
+
+	var fetchShow = function(property, value, parseFetch, callback){
+		parseFetch.equalTo(property, value).find({
+			success: function(obj){
+				console.log(obj);
+				callback(obj[0]);	// What should I do from here?
+			},
+			error: function(err){
+				console.log(err);
+			}
+		});
+	};
+
+	// 4. UPDATE
+	var updateShow = function(obj, property, value){
+		obj.set(property, value);
+		obj.save();
+	}
+
+	// 5. DELETE
+	var deleteShow = function(obj){
+		obj.destroy();
+	}	
+
+	var init = function(){
+
+		initParse();
+
+		// fetchShow('title', 'The Sopranos', showQuery, function(obj){
+		// 	updateShow(obj, 'title', 'Sopranos');
+		// });
+		fetchShow('title', 'Sopranos', showQuery, function(obj){
+			deleteShow(obj);
+		});
+
+		// saveShow(tvShows, {
+		// 	title: 'Dexter',
+		// 	channel: 'ABC',
+		// 	genre: ['Thriller', 'Horror']
+		// });
+		// saveShow(tvShows, {
+		// 	title: 'The Sopranos',
+		// 	channel: 'HBO',
+		// 	genre: ['Drama']
+		// });
+		// saveShow(tvShows, {
+		// 	title: 'Mad Men',
+		// 	channel: 'AMC',
+		// 	genre: ['Drama'],
+		// 	number_of_seasons: 7
+		// });
+		// saveShow(tvShows, {
+		// 	title: 'Game of Thrones',
+		// 	channel: 'HBO',
+		// 	genre: ['Drama', 'Medieval'],
+		// 	number_of_seasons: 5
+		// });		
+
+		// fetchAllShows(showQuery);
+
+		// fetchShow('number_of_seasons', 5, showQuery);
+
+		// updateShow('Mad Men', tvShows, showQuery);
+
+		// deleteShow('Game of Thrones', tvShows, showQuery);
+	}
+
+	return {
+		init: init
+	};
+})();
+
+/* Wait for all elements on the page to load */
+window.addEventListener('DOMContentLoaded', app.main.init);
